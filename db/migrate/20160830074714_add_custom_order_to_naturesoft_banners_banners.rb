63e271eabcf1a3d7520bdd925f6a9bceb891a738
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-09-05 01:49:28 -0700

    fix bug data style for custom order

--- db/migrate/20160830074714_add_custom_order_to_naturesoft_banners_banners.rb
+++ db/migrate/20160830074714_add_custom_order_to_naturesoft_banners_banners.rb
@@ -1,5 +1,5 @@
 class AddCustomOrderToNaturesoftBannersBanners < ActiveRecord::Migration[5.0]
   def change
-    add_column :naturesoft_banners_banners, :custom_order, :string
+    add_column :naturesoft_banners_banners, :custom_order, :integer, default: 0
   end
 end
